b7bff6f699bcff81f74867aa9ae3ab10bfa9c3ee
Mikhail <mihailtimanov@gmail.com>
2022-07-24 10:47:36 +0300

    Добавление сущностей, настройка БД

---
+++ README.md
@@ -0,0 +1,18 @@
+![img.png](img.png)
+
+Что хранится в каждом пакете:
+
+src\main\java\com\boots\config — классы с конфигурациями для MVC (MvcConfig) и безопасности (WebSecurityConfig);
+
+src\main\java\com\boots\controller — классы с контроллерами;
+
+src\main\java\com\boots\entity — классы с моделями;
+
+src\main\java\com\boots\repository — интерфейсы репозиториев;
+
+src\main\java\com\boots\service — классы c сервисами для моделей;
+
+src\main\webapp\resources — статические объекты: js, css, img;
+
+src\main\webapp\WEB-INF\jsp — представления в виде файлов .jsp.
+
---
+++ src/main/java/com/timanov/SpringSecurity/entity/User.java
@@ -0,0 +1,100 @@
+package com.timanov.SpringSecurity.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@Size(min2, message = "Не меньше 5 знаков")
+    private String username;
+
+    //@Size(min2, message = "Не меньше 5 знаков")
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
